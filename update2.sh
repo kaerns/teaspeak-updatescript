1b6bc2efca3c847463faa160c07ff7482a0dbc0a
essem <12894598+essemX@users.noreply.github.com>
2018-06-12 01:50:58 +0200

    Update update2.sh
    add: backing up config and database now
    add: making *.sh files in the current directory executable for the owner

--- update2.sh
+++ update2.sh
@@ -3,10 +3,15 @@ echo "# TeaSpeak Updater by essemX (github.com/essemX/teaspeak-updatescript)"
 echo "# Getting version..."
 version=$(curl -s -S -k https://repo.teaspeak.de/latest)
 echo "# Newest version is ${version}"
-requesturl="https://repo.teaspeak.de/server/linux/x64/TeaSpeak-${version}.tar.gz"
+requesturl="https://repo.teaspeak.de/server/linux/x64/TeaSpeak-${version}.tar.g$
 echo "# Downloading ${requesturl}"
 curl -s -S "$requesturl" -o updatefile.tar.gz
-echo "# Unzipping and removing updatefile.tar.gz"
+echo "# Backing up config and database"
+cp config.yml config.yml.old
+cp TeaData.sqlite TeaData.sqlite.old
+echo "# Unpacking and removing .tar.gz"
 tar -xzf updatefile.tar.gz
 rm updatefile.tar.gz
+echo "# Making scripts executable"
+chmod u+x *.sh
 echo "# TeaSpeak should be now updated to ${version}"
