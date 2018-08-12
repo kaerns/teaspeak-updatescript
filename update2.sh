75a01eaa5c82106e5295c312215021ab4933586b
essem <12894598+essemX@users.noreply.github.com>
2018-08-12 04:25:03 +0200

    added environment detection
    +script now detects which version it should download
    *better readable format

--- update2.sh
+++ update2.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 echo "# TeaSpeak Updater by essemX (github.com/essemX/teaspeak-updatescript)"
+
 echo "# Getting version..."
 version=$(curl -s -S -k https://repo.teaspeak.de/latest)
 echo "# Newest version is ${version}"
-requesturl="https://repo.teaspeak.de/server/linux/amd64/TeaSpeak-${version}.tar.gz"
+
+if [ `getconf LONG_BIT` = "64" ]
+then
+        echo "# Detected an 64 bit environment"
+        requesturl="https://repo.teaspeak.de/server/linux/amd64/TeaSpeak-${version}.tar.gz"
+else
+        echo "# Detected an 32 bit environment"
+        requesturl="https://repo.teaspeak.de/server/linux/x86/TeaSpeak-${version}.tar.gz"
+fi
+
 echo "# Downloading ${requesturl}"
 curl -s -S "$requesturl" -o updatefile.tar.gz
+
 echo "# Backing up config and database"
 cp config.yml config.yml.old
 cp TeaData.sqlite TeaData.sqlite.old
+
 echo "# Unpacking and removing .tar.gz"
 tar -xzf updatefile.tar.gz
 rm updatefile.tar.gz
+
 echo "# Making scripts executable"
 chmod u+x *.sh
+
 echo "# TeaSpeak should be now updated to ${version}"
